711338499ab4e75e6740c901e78fa80dc82b0d29
osakoh <macmive@gmail.com>
2021-06-30 21:13:22 +0100

    added docstrings to document functions

--- py_task/main.py
+++ py_task/main.py
@@ -1,18 +1,25 @@
 import pandas as pd
 
+# folder containing data
 path = '../data'
 
 
 # step 1: create a function which reads a csv and transforms into a dataframe
-# df = pd.read_csv(f'{path}/input/anonoymous_data.csv')
-
-
 def load_file(file_path, pds):
+    """
+    :param file_path: ref to folder containing the csv file
+    :param pds: ref to the Pandas package
+    :return: a dataframe
+    """
     return pds.read_csv(f'{file_path}/input/anonoymous_data.csv')
 
 
 # step 3: create a new dataframe which counts the number of times each emotion appears
 def count_emotion(df):
+    """
+    :param df: ref to a dataframe
+    :return: a sorted dataframe
+    """
     # delete log_id column
     df = df.head(10).drop(['log_id', 'user_id'], axis=1)
     # extract first 4 columns
@@ -26,6 +33,11 @@ def count_emotion(df):
 
 # step 4: Export the new dataframe to a json file in the js_task folder to be used in part 2 - visualising
 def to_json(df, output_path):
+    """
+    :param df: ref to a dataframe
+    :param output_path: path to save the json file
+    :return: a data json file
+    """
     # create JSON file
     json_file = df.to_json(orient='records')
 
